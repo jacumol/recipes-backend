839a92f5add5dc7bea118d0a4da7f09c8787c3ce
Jairo Cuartas <jairo@headcount365.com>
2024-03-11 14:54:40 -0500

    Several steps.
    Signed-off-by: Jairo Cuartas <jairocuartas@gmail.com>

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
     'core',
+    'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +131,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
